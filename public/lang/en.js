263149e1be8045f8eb26934cba73f9b174c905cd
nguyen.ngoc.trungc <nguyen.ngoc.trung-c@sun-asterisk.com>
2026-06-19 17:08:31 +0700

    feat: integrate i18n support with English and Vietnamese translations, add configuration for internationalization

--- public/lang/en.js
+++
@@ -1,7 +0,0 @@
-export default {
-    translation: {
-        hello: 'Hello',
-        welcome_to_next_js: 'Welcome to Next.js!',
-        learn_more: 'Learn more',
-    },
-};
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,7 +16,9 @@ async function bootstrap() {
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, documentFactory);
 
-  await app.listen(process.env.PORT ?? 3000);
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('PORT', 3000);
+  await app.listen(port);
 }
 
 bootstrap();
